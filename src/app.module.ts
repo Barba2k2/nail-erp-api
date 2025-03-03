9a49dd927d1ba9ce30e3e4e15a003c628ddd654d
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-03 20:11:15 -0300

    Configures app modules and dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProfessionalsModule } from './professionals/professionals.module';
+import { ClientsModule } from './clients/clients.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    ProfessionalsModule,
+    ClientsModule,
+    AppointmentsModule,
+    ServicesModule,
+  ],
 })
 export class AppModule {}
