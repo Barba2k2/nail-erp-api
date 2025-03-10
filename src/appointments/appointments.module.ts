fa222acb88fd0b56fa4b664ae575b8f50b9f636e
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-10 16:55:19 -0300

    Integrates notifications in appointments module

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -3,13 +3,16 @@ import { AppointmentsController } from './appointments.controller';
 import { AppointmentsService } from './appointments.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { SettingsModule } from 'src/settings/settings.module';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
   imports: [
     PrismaModule,
     SettingsModule,
+    NotificationsModule,
   ],
   controllers: [AppointmentsController],
   providers: [AppointmentsService],
+  exports: [AppointmentsService],
 })
 export class AppointmentsModule {}
