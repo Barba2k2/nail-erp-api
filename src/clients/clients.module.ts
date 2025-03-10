84c3bcd5a52a3036369fb5a677ab7258e77ca5c3
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-10 16:55:38 -0300

    Adds client history controllers and service

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import {
+  AdminClientHistoryController,
+  ClientHistoryController,
+} from './history/client-history.controller';
+import { ClientHistoryService } from './history/client-history.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [ClientsController],
-  providers: [ClientsService],
+  controllers: [
+    ClientsController,
+    ClientHistoryController,
+    AdminClientHistoryController,
+  ],
+  providers: [ClientsService, ClientHistoryService],
 })
 export class ClientsModule {}
