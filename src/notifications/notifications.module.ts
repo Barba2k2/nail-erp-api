59475fed6f7467c127eb8caf0db0b37cd4f7d4fb
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-10 20:50:52 -0300

    Adds admin notifications controller to module

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-import { NotificationsController } from './notifications.controller';
+import { NotificationsController, AdminNotificationsController } from './notifications.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { NotificationSchedulerService } from './notification-scheduler.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [NotificationsController],
+  controllers: [NotificationsController, AdminNotificationsController],
   providers: [NotificationsService, NotificationSchedulerService],
   exports: [NotificationsService, NotificationSchedulerService],
 })
