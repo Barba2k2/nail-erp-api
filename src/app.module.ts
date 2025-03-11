297de8b101dfcfb08584d1e0ca620f3f226c7192
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-11 11:53:30 -0300

    Adds EventEmitter module with configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,20 @@ import { ServicesModule } from './services/services.module';
 import { SettingsModule } from './settings/settings.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { NotificationsModule } from './notifications/notifications.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    EventEmitterModule.forRoot({
+      wildcard: false,
+      delimiter: '.',
+      maxListeners: 10,
+      ignoreErrors: false,
+      verboseMemoryLeak: true,
     }),
     ScheduleModule.forRoot(),
     PrismaModule,
