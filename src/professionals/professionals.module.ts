e35c1561c7b549778b368844ddfe3d1b44ed1693
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-11 11:06:16 -0300

    Adds admin clients functionality to professionals module

--- src/professionals/professionals.module.ts
+++ src/professionals/professionals.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProfessionalsController } from './professionals.controller';
 import { ProfessionalsService } from './professionals.service';
+import { AdminClientsController } from './clients/admin-clients.controller';
+import { AdminClientsService } from './clients/admin-clients.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [ProfessionalsController],
-  providers: [ProfessionalsService],
+  controllers: [ProfessionalsController, AdminClientsController],
+  providers: [ProfessionalsService, AdminClientsService],
 })
 export class ProfessionalsModule {}
