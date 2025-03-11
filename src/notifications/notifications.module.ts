f0ccaecbb7a72f8428e450ff077851a7d2d74b06
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-11 11:52:53 -0300

    Adds appointment notification listener to module

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -3,11 +3,16 @@ import { NotificationsService } from './notifications.service';
 import { NotificationsController, AdminNotificationsController } from './notifications.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { NotificationSchedulerService } from './notification-scheduler.service';
+import { AppointmentNotificationListener } from './listeners/appointment-notification.listener';
 
 @Module({
   imports: [PrismaModule],
   controllers: [NotificationsController, AdminNotificationsController],
-  providers: [NotificationsService, NotificationSchedulerService],
+  providers: [
+    NotificationsService,
+    NotificationSchedulerService,
+    AppointmentNotificationListener,
+  ],
   exports: [NotificationsService, NotificationSchedulerService],
 })
 export class NotificationsModule {}
