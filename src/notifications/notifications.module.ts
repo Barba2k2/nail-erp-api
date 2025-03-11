799f16567d10ce5e33c2afe78cfa6005b41f0c86
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-11 14:31:17 -0300

    Adds notification templates functionality

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,18 +1,32 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-import { NotificationsController, AdminNotificationsController } from './notifications.controller';
+import {
+  NotificationsController,
+  AdminNotificationsController,
+} from './notifications.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { NotificationSchedulerService } from './notification-scheduler.service';
 import { AppointmentNotificationListener } from './listeners/appointment-notification.listener';
+import { NotificationTemplatesController } from './template/notification-templates.controller';
+import { NotificationTemplatesService } from './template/notification-templates.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [NotificationsController, AdminNotificationsController],
+  controllers: [
+    NotificationsController,
+    AdminNotificationsController,
+    NotificationTemplatesController,
+  ],
   providers: [
     NotificationsService,
     NotificationSchedulerService,
     AppointmentNotificationListener,
+    NotificationTemplatesService,
   ],
-  exports: [NotificationsService, NotificationSchedulerService],
+  exports: [
+    NotificationsService,
+    NotificationSchedulerService,
+    NotificationTemplatesService,
+  ],
 })
 export class NotificationsModule {}
