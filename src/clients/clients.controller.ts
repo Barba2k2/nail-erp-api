a8c36de72e77fba12186057a14049f391f96a3b0
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-11 11:51:33 -0300

    Adds event emission for appointment actions

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -14,11 +14,16 @@ import { RolesGuard } from '../auth/decorator/roles.guard';
 import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
 import { Roles } from '../auth/decorator/roles.decorator';
 import { DateUtils } from '../utils/date.utils';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { AppointmentCanceledEvent, AppointmentCreatedEvent, AppointmentRescheduledEvent } from 'src/appointments/events/appointment.events';
 
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('client')
 export class ClientsController {
-  constructor(private readonly clientsService: ClientsService) {}
+  constructor(
+    private readonly clientsService: ClientsService,
+    private readonly eventEmitter: EventEmitter2,
+  ) {}
 
   @Roles('CLIENT')
   @Get('profile')
@@ -46,18 +51,18 @@ export class ClientsController {
   @Roles('CLIENT', 'PROFESSIONAL')
   @Post('appointments')
   async createAppointment(@Req() req, @Body() data: any) {
-    console.log('Req user:', req.user);
-
     if (!req.user || !req.user.id) {
       throw new Error('Usuário não autenticado ou ID ausente no token');
     }
 
-    console.log('User ID:', req.user.id);
-    console.log('Request data:', data);
-
     const appointment = await this.clientsService.createAppointment(
       req.user.id,
       data,
+    );
+
+    this.eventEmitter.emit(
+      'appointment.created',
+      new AppointmentCreatedEvent(appointment.id),
     );
 
     return {
@@ -74,6 +79,11 @@ export class ClientsController {
       data,
     );
 
+    this.eventEmitter.emit(
+      'appointment.rescheduled',
+      new AppointmentRescheduledEvent(appointment.id),
+    );
+
     return {
       ...appointment,
       ...DateUtils.formatAppointmentDateTime(appointment.date),
@@ -84,6 +94,11 @@ export class ClientsController {
   @Delete('appointments/:id')
   async cancelAppointment(@Param('id') id: string) {
     const appointment = await this.clientsService.cancelAppointment(Number(id));
+
+    this.eventEmitter.emit(
+      'appointment.canceled',
+      new AppointmentCanceledEvent(appointment.id),
+    );
 
     return {
       ...appointment,
