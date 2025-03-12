f3f7441cf43e3f1d58a8b0e630a8f9ac10cf2363
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-11 21:08:15 -0300

    Adds business info controllers and service

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { SettingsService } from './settings.service';
 import { SettingsController } from './settings.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { BusinessInfoController, PublicBusinessInfoController } from './business-info/business-info.controller';
+import { BusinessInfoService } from './business-info/business-info.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [SettingsController],
-  providers: [SettingsService],
-  exports: [SettingsService], 
+  controllers: [
+    SettingsController,
+    BusinessInfoController,
+    PublicBusinessInfoController,
+  ],
+  providers: [SettingsService, BusinessInfoService],
+  exports: [SettingsService, BusinessInfoService],
 })
 export class SettingsModule {}
