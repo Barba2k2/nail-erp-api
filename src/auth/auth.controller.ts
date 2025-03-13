27bd70d4c442270b1538842d25a3dbfcba548699
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-13 14:11:31 -0300

    Enhances auth controller with validation and rate limiting

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,46 +6,59 @@ import {
   Get,
   HttpCode,
   HttpStatus,
+  UseGuards,
+  UseFilters,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RecoverPasswordDto } from './dto/recover-password.dto';
 import { UserRole } from '@prisma/client';
 import { ResetPasswordDto } from './dto/reset-password.dto';
 import { VerifyTokenDto } from './dto/verify-token.dto';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { AuthExceptionFilter } from './filtes/auth-exception.filter';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Controller('auth')
+@UseFilters(AuthExceptionFilter)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register/professional')
-  async registerProfessional(@Body() data: any) {
-    data.role = 'PROFESSIONAL';
+  @UseGuards(ThrottlerGuard)
+  async registerProfessional(@Body() data: RegisterDto) {
+    data.role = UserRole.PROFESSIONAL;
     return this.authService.register(data);
   }
 
   @Post('register/client')
-  async registerClient(@Body() data: any) {
-    data.role = 'CLIENT';
+  @UseGuards(ThrottlerGuard)
+  async registerClient(@Body() data: RegisterDto) {
+    data.role = UserRole.CLIENT;
     return this.authService.register(data);
   }
 
   @Post('login/professional')
-  async loginProfessional(@Body() body: any) {
-    return this.authService.loginProfessional(body);
+  @UseGuards(ThrottlerGuard)
+  async loginProfessional(@Body() body: LoginDto) {
+    return this.authService.login(body, UserRole.PROFESSIONAL);
   }
 
   @Post('login/client')
-  async loginClient(@Body() body: any) {
-    return this.authService.loginClient(body);
+  @UseGuards(ThrottlerGuard)
+  async loginClient(@Body() body: LoginDto) {
+    return this.authService.login(body, UserRole.CLIENT);
   }
 
   @Post('recover-password/client')
+  @UseGuards(ThrottlerGuard)
   async recoverClientPassword(@Body() data: RecoverPasswordDto) {
     data.userType = UserRole.CLIENT;
     return this.authService.recoverPassword(data);
   }
 
   @Post('recover-password/professional')
+  @UseGuards(ThrottlerGuard)
   async recoverProfessionalPassword(@Body() data: RecoverPasswordDto) {
     data.userType = UserRole.PROFESSIONAL;
     return this.authService.recoverPassword(data);
@@ -53,11 +66,12 @@ export class AuthController {
 
   @Get('verify-reset-token')
   async verifyResetToken(@Query() query: VerifyTokenDto) {
-    return this.authService.verifyResetToken(query);
+    return this.authService.verifyResetToken(query.token);
   }
 
   @Post('reset-password')
   @HttpCode(HttpStatus.OK)
+  @UseGuards(ThrottlerGuard)
   async resetPassword(@Body() data: ResetPasswordDto) {
     return this.authService.resetPassword(data);
   }
