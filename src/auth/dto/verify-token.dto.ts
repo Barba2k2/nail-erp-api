baf0974957e875b65e29bbff6eb27e6a43756f6e
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-13 14:10:32 -0300

    Fixes Portuguese accent in token validation message

--- src/auth/dto/verify-token.dto.ts
+++ src/auth/dto/verify-token.dto.ts
@@ -2,6 +2,6 @@ import { IsNotEmpty, IsString } from 'class-validator';
 
 export class VerifyTokenDto {
   @IsNotEmpty({ message: 'O token é obrigatório' })
-  @IsString({ message: 'Token invalido' })
+  @IsString({ message: 'Token inválido' })
   token: string;
 }
