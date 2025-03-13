edc892a66b2cd49693728cfede5284c82664c368
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-13 14:09:37 -0300

    Updates validation messages with proper accents

--- src/auth/dto/recover-password.dto.ts
+++ src/auth/dto/recover-password.dto.ts
@@ -1,12 +1,12 @@
-import { UserRole } from "@prisma/client";
-import { IsEmail, IsEnum, IsNotEmpty, IsOptional } from "class-validator";
+import { UserRole } from '@prisma/client';
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
 
-export class RecoverPasswordDto { 
-  @IsNotEmpty({ message: 'O email eh obrigatorio' })
-  @IsEmail({}, { message: 'Email invalido' })
+export class RecoverPasswordDto {
+  @IsNotEmpty({ message: 'O email é obrigatório' })
+  @IsEmail({}, { message: 'Email inválido' })
   email: string;
 
   @IsOptional()
-  @IsEnum(UserRole, { message: 'Tipo de usuario invalido' })
-  userType: UserRole;
+  @IsEnum(UserRole, { message: 'Tipo de usuário inválido' })
+  userType?: UserRole;
 }
